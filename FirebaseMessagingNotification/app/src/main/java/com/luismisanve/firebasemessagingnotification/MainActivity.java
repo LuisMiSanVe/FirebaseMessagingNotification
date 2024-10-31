b196c30a235d57afa5b385ab632faed4c2c364b5
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2024-10-31 08:42:23 +0100

    Add files via upload

---
+++ FirebaseMessagingNotification/app/src/main/java/com/luismisanve/firebasemessagingnotification/MainActivity.java
@@ -0,0 +1,50 @@
+package com.luismisanve.firebasemessagingnotification;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView text = findViewById(R.id.text);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.w("FCM", "Fetching FCM registration token failed", task.getException());
+                        return;
+                    }
+
+                    // Get new FCM registration token
+                    String token = task.getResult();
+                    Log.d("FCM", "FCM Token: " + token);
+
+                    text.setText(token);
+
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(this.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("token", text.getText());
+                    clipboard.setPrimaryClip(clip);
+                });
+    }
+}
