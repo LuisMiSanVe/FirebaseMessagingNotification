23d4ea333f601f1e404b50395d77a9a057e1dfad
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2024-10-31 08:36:24 +0100

    Add files via upload

---
+++ FirebaseMessagingNotificationServer/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace FirebaseMessagingNotificationServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [HttpPost]
+        public void SendNotification(string title, string body, string token)
+        {
+            try
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile("credentials.json")
+                });
+            } catch { }
+
+            var message = new Message()
+            {
+                Notification = new Notification()
+                {
+                    Title = title,
+                    Body = body
+                },
+                Token = token
+            };
+
+            FirebaseMessaging.DefaultInstance.SendAsync(message);
+        }
+    }
+}
